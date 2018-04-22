eea094926c3bad5872d92120650baa835b8d626c
GehuaZhang <zhangehuazee@gmail.com>
2018-04-23 02:56:37 +0800

    regression model update

--- data_to_pdf/HaveAFirstLook.py
+++ data_to_pdf/HaveAFirstLook.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pickle as pkl
 
-pkl_file = open('outcome_21_10_5_5.pkl', 'rb')
+pkl_file = open('marketValueMatrix_21.pkl', 'rb')
 
 a = pkl.load(pkl_file)
 
--- regression/regression.py
+++ regression/regression.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pickle as pkl
-from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 import numpy as np
 
 
@@ -10,11 +10,16 @@ class Regression:
         pkl_file_expos = open('..\exposureMatrix_21_10_10.pkl', 'rb')
         pkl_file_return = open('..\dailyReturnMatrix_21_10_10.pkl', 'rb')
         pkl_file_outcome = open('..\outcome_21_10_5_5.pkl', 'rb')
+        pkl_file_market_value = open('..\marketValueMatrix_21.pkl', 'rb')
         self.df_return = pkl.load(pkl_file_return)
         self.df_expos = pkl.load(pkl_file_expos)
         self.df_outcome = pkl.load(pkl_file_outcome)
+        self.df_market_value = pkl.load(pkl_file_market_value)
 
-    def reset_index(self, df_temp, index_name): # Reset index and remove it from previous dataframe
+    def get_dic_key(self, dict_temp):
+        return
+
+    def reset_index(self, df_temp, index_name):  # Reset index and remove it from previous dataframe
         df_temp.index = df_temp[index_name].apply(lambda x: str(x)).tolist()
         df_temp = df_temp.drop(labels=index_name, axis=1)
         return df_temp
@@ -43,31 +48,38 @@ class Regression:
             print("No Such Trade Date In outcome.pkl.")
             return
 
-
-
         market_label = list(set(df_outcome['MarketValuePortfolio']))
         factor_label = list(set(df_outcome['FactorPortfolio']))
-        df_market = pd.DataFrame(np.zeros((len(symbol_list), len(market_label))), index=symbol_list,
+
+        df_market_label = pd.DataFrame(np.zeros((len(symbol_list), len(market_label))), index=symbol_list,
                                  columns=market_label)
 
+        # 生成关于symbol_list的字典
+        dic_symbol = {}
+        for market_index in market_label:
+            for factor_index in factor_label:
+                dic_symbol[(market_index, factor_index)] = df_outcome['symbols'][(df_outcome["MarketValuePortfolio"] == market_index) & \
+                                                              (df_outcome["FactorPortfolio"] == factor_index)].iloc[0]
+
+        # 把symbol_list中有的元素更新到df_market
+        # 没有在outcome.pkl中的元素，但在expos中的元素，默认所有MV系数为0
+        for symbols in symbol_list:
+            for keys, values in dic_symbol.items():
+                if symbols in values:
+                    market_index, _ = keys
+                    df_market_label[market_index].loc[symbols] = 1.0
+                    break
+                else:
+                    continue
         """
-        dic = {}
-        for x in market_label:
-            for y in factor_label:
-                dic[(x,y)] = df_outcome['symbols'][(df_outcome["MarketValuePortfolio"]==x) and (df_outcome['FactorPortfolio']==y)].iloc[1]
-
-        print(dic)
-
-        """
-
         for symbols in symbol_list:
             a = df_outcome['MarketValuePortfolio'][df_outcome['symbols'].apply(lambda x: symbols in x)]
             if a.index.isin([trade_date]).any() :
                 market_index = df_outcome['MarketValuePortfolio'][df_outcome['symbols'].apply(lambda x: symbols in x)].loc[trade_date]  # 查找该股票对应的市值值
                 df_market[market_index].loc[symbols] = 1.0
                 continue
-
-        return df_market
+        """
+        return df_market_label
 
     def pnl_filter(self, trade_date, symbol_list):
         df_return = self.reset_index(self.df_return, index_name="trade_date")
@@ -78,28 +90,44 @@ class Regression:
         df_pnl = df_return[symbol_list].loc[trade_date]
         return df_pnl
 
+    def weight_fither(self, trade_date, symbol_list):
+        df_market_value = self.reset_index(self.df_market_value, index_name="trade_date")
+        if not df_market_value.index.contains(trade_date):
+            print("No Such Trade Date In marketValueMatrix.pkl.")
+            return
+
+        df_market_value = df_market_value[symbol_list].loc[trade_date]
+        return df_market_value
+
     def linear_model(self):
         df_expos = self.expos_initialize()
         avail_date = df_expos.index.tolist()
         avail_symbol = df_expos.columns.tolist()
         df_expos_T = df_expos.T  #  转置
 
-        print(avail_symbol)
+        df_model = pd.DataFrame(index=avail_date, columns=["model_coef","intercept"])
 
         for date_temp in avail_date:
-            print("linear: "+date_temp)
-            sr_pnl = self.pnl_filter(date_temp, symbol_list=avail_symbol)
-            sr_market = self.market_filter(date_temp, symbol_list=avail_symbol)
-            #sr_expos = df_expos_T[date_temp]
+            print("trade_date: "+date_temp)
+            sr_pnl = self.pnl_filter(date_temp, symbol_list=avail_symbol)                 # StockNumber x 1
+            sr_expos = df_expos_T[date_temp]                                              # StockNumber x 1
+            df_market_label = self.market_filter(date_temp, symbol_list=avail_symbol)     # StockNumber x MarketIndexNumber
+            df_market_value = self.weight_fither(date_temp, symbol_list=avail_symbol)     # StockNumber x 1
 
-            #print(len(sr_pnl)==len(sr_market))
-            print(sr_market)
-        # df_pnl = self.pnl_filter()
-        #reg_model = linear_model.LinearRegression()
-        #reg_model.fit()
+            df_x = pd.concat([sr_expos, df_market_label], axis=1)
+            df_y = sr_pnl
+            df_weight = df_market_value**(-0.5)
+            df_weight = df_weight.fillna(0) #NaN值被替代为0
 
+            # Regression
+            regre_model = LinearRegression()
+            regre_model.fit(df_x, df_y, sample_weight=df_weight)
 
+            df_model["model_coef"].loc[date_temp] = [regre_model.coef_.tolist()]
+            df_model["intercept"].loc[date_temp] = [regre_model.intercept_.tolist()]
+
+        return df_model
+
+pd.options.mode.chained_assignment = None
 a = Regression()
-#b = a.market_filter(trade_date="20180223", symbol_list=["000001.SZ", "000002.SZ", "601669.SH"])
-c = a.linear_model()
-#print(b)
+print(a.linear_model())
